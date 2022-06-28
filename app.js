c4659e05f4faa7025ccb62dba4c20b50509a86eb
Pushkar Verma <pushkar7397@gmail.com>
2022-06-28 12:46:26 +0530

    connect mongodb to existing project

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017//todolistDB");
+mongoose.connect("mongodb://localhost:27017//todolistDB"); //Connect to mongodb using mongoose
 
 app.get("/", function(req, res) {
 
