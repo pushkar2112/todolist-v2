b5fd4fade5c3d184c8ebc337d14a062d568d920e
Pushkar Verma <pushkar7397@gmail.com>
2022-06-28 14:05:40 +0530

    Add new item to the list

--- app.js
+++ app.js
@@ -59,15 +59,22 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const item  = new Item({
+    name: itemName
+  });
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  item.save();
+
+  res.redirect("/")
+
+  // if (req.body.list === "Work") {
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // } else {
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
 });
 
 app.get("/work", function(req,res){
